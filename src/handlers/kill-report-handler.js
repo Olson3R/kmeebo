23057ebb32cc1575ea5501e18e90c5f7bd1f980b
Scott Olson <scootles.olson@gmail.com>
2022-09-21 11:01:22 -0500

    Limit channels and fix status

--- src/handlers/kill-report-handler.js
+++ src/handlers/kill-report-handler.js
@@ -1,9 +1,14 @@
 const axios = require('axios')
 const fs = require('fs')
 const fsp = require('fs/promises')
+const _ = require('lodash')
 
 const parseKillReport = require('../services/kill-report-parser')
 
+const CHANNEL_IDS = [
+  '787766810044399616', // SS#bot-dev
+  '1020372930384908288' // HTP#new-km-bot-testing
+]
 const IMAGE_CONTENT_TYPE = /^image\//
 
 const getImageAttachment = async (url) => {
@@ -12,8 +17,11 @@ const getImageAttachment = async (url) => {
 }
 
 const killReportHandler = async message => {
+  if (!CHANNEL_IDS.includes(message.channel.id)) return
+
   console.log(`${message.author.tag} in #${message.channel.name} created a message. ${message.attachments.size}`)
   if (message.attachments.size > 0) {
+    const killReports = []
     for (const attachment of message.attachments.values()) {
       if (!IMAGE_CONTENT_TYPE.test(attachment.contentType)) continue
 
@@ -21,27 +29,40 @@ const killReportHandler = async message => {
       const submittedBy = message.author.tag
       console.log(`\timage attachment: ${attachment.contentType} - ${attachment.url}`)
       const imageData = await getImageAttachment(attachment.url)
-      // const imagePath = `${KILL_REPORT_DIR}/${guildId}`
-      // if (!fs.existsSync(imagePath)) {
-      //   fs.mkdirSync(imagePath, { recursive: true })
-      // }
-      // await fsp.writeFile(`${imagePath}/${attachment.name}`, imageData)
+
       try {
         const killReport = await parseKillReport(guildId, submittedBy, attachment.name, imageData, { url: attachment.url })
         console.log('RESULTS', killReport)
-        if (killReport.duplicate) {
-          message.react('©️')
-        }
-        else if(killReport.status !== 'SUCCESS') {
-          message.react('❌')
-        }
-        else {
-          message.react('✅')
-        }
+        killReports.push(killReport)
       }
       catch(e) {
         console.error(e)
         message.react('❌')
+      }
+    }
+
+    const status = _.chain(killReports)
+      .map(km => km.duplicate ? 'DUPLICATE' : km.status)
+      .reduce(
+        (result, km) => {
+          if (result === 'ERROR' || km === 'ERROR') return 'ERROR'
+          if (result === 'DUPLICATE' || km) return 'DUPLICATE'
+          return 'SUCCESS'
+        },
+        'SUCCESS'
+      )
+      .value()
+    switch (status) {
+      case 'DUPLICATE': {
+        message.react('©️')
+        break
+      }
+      case 'ERROR': {
+        message.react('❌')
+        break
+      }
+      default: {
+        message.react('✅')
       }
     }
   }
