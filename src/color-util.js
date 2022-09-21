d72cd7f5ec320c59f28116b65e9fee68ea97081b
Scott Olson <scootles.olson@gmail.com>
2022-09-21 12:33:32 -0500

    Respond with results

---
+++ src/color-util.js
@@ -0,0 +1,5 @@
+module.exports = {
+  green: 8311585,
+  red: 13632027,
+  yellow: 16776960
+}
--- src/handlers/kill-report-handler.js
+++ src/handlers/kill-report-handler.js
@@ -3,6 +3,7 @@ const fs = require('fs')
 const fsp = require('fs/promises')
 const _ = require('lodash')
 
+const colors = require('../color-util')
 const parseKillReport = require('../services/kill-report-parser')
 
 const CHANNEL_IDS = [
@@ -14,6 +15,17 @@ const IMAGE_CONTENT_TYPE = /^image\//
 const getImageAttachment = async (url) => {
   const response = await axios.get(url, { responseType: 'arraybuffer'})
   return Buffer.from(response.data, 'binary')
+}
+
+const getColor = km => {
+  if (km.status === 'ERROR') return colors.red
+  if (km.duplicate) return colors.yellow
+  return colors.green
+}
+
+const getDescription = km => {
+  if (km.status === 'ERROR') return `${km.id}\n${km.statusMessage ?? 'Unkown error'}`
+  return `${km.id}\n${km.killReportId} | ${km.isk.toLocaleString()} ISK | [${km.finalBlowCorp ?? '???'}] ${km.finalBlowName}`
 }
 
 const killReportHandler = async message => {
@@ -32,7 +44,7 @@ const killReportHandler = async message => {
 
       try {
         const killReport = await parseKillReport(guildId, submittedBy, attachment.name, imageData, { url: attachment.url })
-        console.log('RESULTS', killReport)
+        // console.log('RESULTS', killReport)
         killReports.push(killReport)
       }
       catch(e) {
@@ -46,25 +58,30 @@ const killReportHandler = async message => {
       .reduce(
         (result, km) => {
           if (result === 'ERROR' || km === 'ERROR') return 'ERROR'
-          if (result === 'DUPLICATE' || km) return 'DUPLICATE'
+          if (result === 'DUPLICATE' || km === 'DUPLICATE') return 'DUPLICATE'
           return 'SUCCESS'
         },
         'SUCCESS'
       )
       .value()
-    switch (status) {
-      case 'DUPLICATE': {
-        message.react('©️')
-        break
-      }
-      case 'ERROR': {
-        message.react('❌')
-        break
-      }
-      default: {
-        message.react('✅')
-      }
+    console.log('STATUSES', status)
+    if (status === 'DUPLICATE') {
+      await message.react('©️')
     }
+    if (status === 'ERROR') {
+      await message.react('❌')
+    }
+    else {
+      await message.react('✅')
+    }
+
+    const embeds = _.map(killReports, km => ({
+      color: getColor(km),
+      description: getDescription(km)
+    }))
+    // const embeds = [{ description: 'this is a test' }]
+    console.log('EEEE', embeds)
+    await message.reply({ embeds })
   }
 }
 
