26a7e0a1d029aaafc847a1d54e9c1c61f7032d12
Scott Olson <scootles.olson@gmail.com>
2022-09-29 01:09:45 -0500

    Perm check

--- src/commands/admin-setup-channel.js
+++ src/commands/admin-setup-channel.js
@@ -1,8 +1,10 @@
+const { PermissionsBitField } = require('discord.js')
 const _ = require('lodash')
 
 const colors = require('../color-util')
 const { isAdmin } = require('../util')
 const { Channel } = require('../models')
+const logger = require('../services/logger')
 
 const adminSetupChannel = async (interaction) => {
   const updatedBy = interaction.user.tag
@@ -30,11 +32,23 @@ const adminSetupChannel = async (interaction) => {
     channel.killTag = killTag
     await channel.save()
 
+    const perms = _.filter([
+      {name: 'AddReactions', value: interaction.appPermissions.has(PermissionsBitField.Flags.AddReactions) },
+      {name: 'ReadMessageHistory', value: interaction.appPermissions.has(PermissionsBitField.Flags.ReadMessageHistory) },
+      {name: 'AttachFiles', value: interaction.appPermissions.has(PermissionsBitField.Flags.AttachFiles) },
+      {name: 'CreatePrivateThreads', value: interaction.appPermissions.has(PermissionsBitField.Flags.CreatePrivateThreads) },
+      {name: 'CreatePublicThreads', value: interaction.appPermissions.has(PermissionsBitField.Flags.CreatePublicThreads) },
+      {name: 'EmbedLinks', value: interaction.appPermissions.has(PermissionsBitField.Flags.EmbedLinks) },
+      {name: 'SendMessages', value: interaction.appPermissions.has(PermissionsBitField.Flags.SendMessages) },
+      {name: 'SendMessagesInThreads', value: interaction.appPermissions.has(PermissionsBitField.Flags.SendMessagesInThreads) },
+    ], { value: false }).map(p => p.name)
+// logger.info('PERMSSS', { channel:  interaction.channel })
     const fields = [
       { name: 'Name', value: interaction.channel.name },
       { name: 'Id', value: channelId },
-      { name: 'Kill Tag', value: killTag ?? '*None*' }
+      { name: 'Kill Tag', value: killTag ?? '*None*' },
     ]
+    if (perms.length > 0) fields.push({ name: 'Missing Channel Permissions', value: perms.join(', ') })
 
     const ending = _.isNil(killTag) ? 'without a kill tag' : `with kill tag \`${killTag}\``
     const embed = {
