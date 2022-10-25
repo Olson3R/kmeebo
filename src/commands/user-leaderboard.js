261f3b952ab9e783c5f5bcd99f2da55cba82cab7
Scott Olson <scootles.olson@gmail.com>
2022-10-24 23:12:25 -0500

    Add scoring for user leaderboard

--- src/commands/user-leaderboard.js
+++ src/commands/user-leaderboard.js
@@ -15,10 +15,17 @@ const formatNumber = (text) => {
 const userLeaderboard = async (interaction) => {
   const guildId = interaction.guildId
   const period = interaction.options.getString('period') ?? 'current-month'
+  const scoring = interaction.options.getString('scoring') ?? 'final-blow'
   const killTag = interaction.options.getString('kill-tag')
+  const SCORING = {
+    'final-blow': { text: 'Final Blow', joinCondition: 'KillReports.finalBlowName = Pilots.name' },
+    'top-damage': { text: 'Top Damage', joinCondition: 'KillReports.topDamageName = Pilots.name' },
+    'final-blow-or-top-damage': { text: 'Final Blow or Top Damage', joinCondition: 'KillReports.finalBlowName = Pilots.name OR KillReports.topDamageName = Pilots.name' }
+  }
 
 
   let periodName = 'Lifetime'
+  let scoringData = SCORING[scoring]
   const replacements = { guildId }
   if (killTag) {
     replacements.killTag = killTag
@@ -35,7 +42,7 @@ const userLeaderboard = async (interaction) => {
   }
   const leaderboard = await sequelize.query(
     'SELECT Pilots.discordTag, COUNT(*) as `kills`, SUM(KillReports.isk) as `isk`' +
-    ' FROM Pilots INNER JOIN KillReports ON KillReports.finalBlowName = Pilots.name' +
+    ` FROM Pilots INNER JOIN KillReports ON ${scoringData.joinCondition}` +
     ' WHERE Pilots.guildId = :guildId AND KillReports.guildId = :guildId AND KillReports.status = \'SUCCESS\'' +
     (replacements.startDt ? ' AND KillReports.killedAt >= :startDt' : '') +
     (replacements.endDt ? ' AND KillReports.killedAt < :endDt' : '') +
@@ -49,11 +56,12 @@ const userLeaderboard = async (interaction) => {
     }
   )
 
+  const results = _.map(leaderboard, (row, index) => `**${index + 1}. ${row.discordTag}** ${formatNumber(row.isk)} Isk (Kills: ${formatNumber(row.kills)})`).join('\n')
   const embed = {
     type: 'rich',
     color: colors.green,
     title: `${periodName} User Leaderboard${killTag ? ` For Kill Tag ${killTag}` : ''}`,
-    description: _.map(leaderboard, (row, index) => `**${index + 1}. ${row.discordTag}** ${formatNumber(row.isk)} Isk (Kills: ${formatNumber(row.kills)})`).join('\n')
+    description: `Scored based on ${scoringData.text}\n\n${results}`
   }
 
   await interaction.reply({ embeds: [embed] })
