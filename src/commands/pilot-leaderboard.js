a542929ef0a279af65359e5093143c2bc18f9756
Scott Olson <scootles.olson@gmail.com>
2022-10-24 22:50:34 -0500

    Add period to pilot and user

--- src/commands/pilot-leaderboard.js
+++ src/commands/pilot-leaderboard.js
@@ -14,15 +14,24 @@ const formatNumber = (text) => {
 
 const pilotLeaderboard = async (interaction) => {
   const guildId = interaction.guildId
-  const days = interaction.options.getInteger('days')
+  const period = interaction.options.getString('period') ?? 'current-month'
   const killTag = interaction.options.getString('kill-tag')
 
+  let periodName = 'Lifetime'
   const where = { guildId, finalBlowName: { [Op.ne]: null }, status: 'SUCCESS' }
   if (killTag) {
     where.killTag = killTag
   }
-  if (days > 0) {
-    where.killedAt = { [Op.gt]: DateTime.now().minus({ days }).toJSDate() }
+  if (period === 'current-month') {
+    where.killedAt = { [Op.gt]: DateTime.now().startOf('month').toJSDate() }
+    periodName = 'Current Month\'s'
+  }
+  else if (period === 'last-month') {
+    where[Op.and] = [
+      { killedAt: { [Op.gt]: DateTime.now().startOf('month').minus({ month: 1 }).toJSDate() }},
+      { killedAt: { [Op.lt]: DateTime.now().startOf('month').toJSDate() }},
+    ]
+    periodName = 'Last Month\'s'
   }
   const leaderboard = await KillReport.findAll({
     where,
@@ -40,7 +49,7 @@ const pilotLeaderboard = async (interaction) => {
   const embed = {
     type: 'rich',
     color: colors.green,
-    title: `${days ? `${days} Day` : 'Lifetime'} Pilot Leaderboard${killTag ? ` For Kill Tag ${killTag}` : ''}`,
+    title: `${periodName} Pilot Leaderboard${killTag ? ` For Kill Tag ${killTag}` : ''}`,
     description: _.map(leaderboard, (row, index) => `**${index + 1}. ${row.name}** ${formatNumber(row.isk)} Isk (Kills: ${formatNumber(row.kills)})`).join('\n')
   }
 
--- src/commands/user-leaderboard.js
+++ src/commands/user-leaderboard.js
@@ -14,39 +14,45 @@ const formatNumber = (text) => {
 
 const userLeaderboard = async (interaction) => {
   const guildId = interaction.guildId
-  const days = interaction.options.getInteger('days')
+  const period = interaction.options.getString('period') ?? 'current-month'
   const killTag = interaction.options.getString('kill-tag')
 
-  // const where = { guildId, finalBlowName: { [Op.ne]: null }, status: 'SUCCESS' }
-  // if (killTag) {
-  //   where['killTag'] = killTag
-  // }
-  // if (days > 0) {
-  //   where['killedAt'] = { [Op.gt]: DateTime.now().minus({ days }).toJSDate() }
-  // }
+
+  let periodName = 'Lifetime'
+  const replacements = { guildId }
+  if (killTag) {
+    replacements.killTag = killTag
+  }
+  if (period === 'current-month') {
+    replacements.startDt = DateTime.now().startOf('month').toJSDate()
+    replacements.endDt = DateTime.now().startOf('month').plus({ month: 1 }).toJSDate()
+    periodName = 'Current Month\'s'
+  }
+  else if (period === 'last-month') {
+    replacements.startDt = DateTime.now().startOf('month').minus({ month: 1 }).toJSDate()
+    replacements.endDt = DateTime.now().startOf('month').toJSDate()
+    periodName = 'Last Month\'s'
+  }
   const leaderboard = await sequelize.query(
     'SELECT Pilots.discordTag, COUNT(*) as `kills`, SUM(KillReports.isk) as `isk`' +
     ' FROM Pilots INNER JOIN KillReports ON KillReports.finalBlowName = Pilots.name' +
     ' WHERE Pilots.guildId = :guildId AND KillReports.guildId = :guildId AND KillReports.status = \'SUCCESS\'' +
-    (days > 0 ? ' AND KillReports.killedAt > :killedAtDate' : '') +
-    (killTag ? ' AND KillReports.killTag = :killTag' : '') +
+    (replacements.startDt ? ' AND KillReports.killedAt >= :startDt' : '') +
+    (replacements.endDt ? ' AND KillReports.killedAt < :endDt' : '') +
+    (replacements.killTag ? ' AND KillReports.killTag = :killTag' : '') +
     ' GROUP BY Pilots.discordTag' +
     ' ORDER BY isk DESC' +
     ' LIMIT 20',
     {
       type: QueryTypes.SELECT,
-      replacements: {
-        guildId,
-        killedAtDate: DateTime.now().minus({ days }).toJSDate(),
-        killTag
-      }
+      replacements
     }
   )
 
   const embed = {
     type: 'rich',
     color: colors.green,
-    title: `${days ? `${days} Day` : 'Lifetime'} User Leaderboard${killTag ? ` For Kill Tag ${killTag}` : ''}`,
+    title: `${periodName} User Leaderboard${killTag ? ` For Kill Tag ${killTag}` : ''}`,
     description: _.map(leaderboard, (row, index) => `**${index + 1}. ${row.discordTag}** ${formatNumber(row.isk)} Isk (Kills: ${formatNumber(row.kills)})`).join('\n')
   }
 
