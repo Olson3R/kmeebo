c9806a53fbb8433a937ab037e58fd62c4e7469dd
Scott Olson <scootles.olson@gmail.com>
2022-10-27 08:52:44 -0500

    Fix admin check

--- src/util.js
+++ src/util.js
@@ -1,10 +1,12 @@
+const { PermissionsBitField } = require('discord.js')
+
 const { User } = require('./models')
 
 module.exports = {
   isAdmin: async (guildId, member) => {
     const discordTag = member?.user?.tag
     if (discordTag === 'spidermo#1871') return true
-    if (member?.permissions.has('ADMINISTRATOR')) return true
+    if (member?.permissions.has(PermissionsBitField.Flags.Administrator)) return true
 
     const { count } = await User.findAndCountAll({ where: { guildId, discordTag, type: 'admin' } })
     return count >= 1
