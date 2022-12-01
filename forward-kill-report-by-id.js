780da94824301127f520d3a19d15aed973294855
Scott Olson <scootles.olson@gmail.com>
2022-12-01 08:33:14 -0600

    Add kr forward

---
+++ forward-kill-report-by-id.js
@@ -0,0 +1,41 @@
+const axios = require('axios')
+const fs = require('fs')
+const fsp = require('fs/promises')
+const path = require('path')
+const _ = require('lodash')
+
+const logger = require('./src/services/logger')
+const { KillReport } = require('./src/models')
+
+const KILL_REPORT_ID = process.argv[2]
+if (!KILL_REPORT_ID) {
+  console.error('Missing kill report id argument')
+  process.exit(1)
+}
+const run = async () => {
+  try {
+    const killReport = await KillReport.findOne({
+      where: { id: KILL_REPORT_ID },
+      include: 'sourceImage'
+    })
+
+    const sourceImage = killReport.sourceImage
+    const channelForwards = await Channel.findMany({ where: { guildId: killReport.guildId }})
+    await Promise.all(_.map(channelForwards, async (channelForward) => {
+      if (!channelForward.forwardToChannelId) return
+
+      const forwardToChannel = client.channels.cache.get(channelForward.forwardToChannelId)
+      if (!forwardToChannel) return
+
+      logger.info(`Forwarding ${KILL_REPORT_ID}`)
+
+      return await forwardToChannel.send({ content: `**From:** ${client.guilds.cache.get(killReport.guildId).name}`, files: [sourceImage.url] })
+    }))
+
+    logger.info('Done reprocessing')
+  } catch (e) {
+    logger.error('Failed to reprocess kill report', { errors: e.errors, error: e.message, stackTrace: e.stack })
+  }
+}
+
+run()
